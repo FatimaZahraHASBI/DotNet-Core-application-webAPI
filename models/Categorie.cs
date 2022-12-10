85d04f7874724437b86308c9c2bf5a7477c52abb
Fatima Zahra Hasbi <hasbi.fatimazahra04@gmail.com>
2022-12-10 14:16:01 +0100

    wepAPI

---
+++ models/Categorie.cs
@@ -0,0 +1,17 @@
+using System.ComponentModel.DataAnnotations;
+public class Category{
+    [Key]
+    [Required]
+    public int CategoryID { get; set; }
+    [Required]
+    [StringLength(35)]
+    public string? CategoryName { get; set; }
+    public virtual ICollection<Product>? Products {get; set; }
+
+public Category(int Id, string Name)
+    {
+        this.CategoryID = Id;
+        this.CategoryName = Name;
+    }
+}
+// Fatima Zahra HASBI
